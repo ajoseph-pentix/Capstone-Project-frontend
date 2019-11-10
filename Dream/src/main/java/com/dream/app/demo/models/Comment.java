93ebc2b0e8e0edfef5503138d2661d81f88aa26a
ajoseph-pentix <pelimont34@outlook.com>
2019-11-10 16:44:12 -0500

    Pushing IntelliJ to master

---
+++ Dream/src/main/java/com/dream/app/demo/models/Comment.java
@@ -0,0 +1,81 @@
+package com.dream.app.demo.models;
+
+import java.util.Date;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Comment_Id", nullable = false)
+    private Long commentId;
+
+    @NotNull
+    @Lob
+    @Column(name = "Text", nullable = false)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "Dream_Post_Id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private DreamPost dreamPost;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "App_User_Id", nullable = false)
+    @JsonIgnore
+    private AppUser appUser;
+
+    @Column(name = "Created_Date", length = 36, nullable = false)
+    private Date createdDate;
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public DreamPost getDreamPost() {
+        return dreamPost;
+    }
+
+    public void setDreamPost(DreamPost dreamPost) {
+        this.dreamPost = dreamPost;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+}
+
