06b3c21fc6c7e9d1bed67e900a51aa56ed97369c
ajoseph-pentix <pelimont34@outlook.com>
2019-11-13 11:52:58 -0500

    Created new nav bar and started on page construction

--- dream_connect_front/src/App.js
+++ dream_connect_front/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import Header from "./Components/navigation/Header" // Header
+//import './App.css';
 import {Route} from "react-router-dom";
 import HomePage from "./Components/Pages/HomePage";
 import LoginPage from "./Components/Pages/LoginPage";
 
-const App = () => (
-  <div className="ui container"> //to add margins to page
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
-    </div>
-  );
+export default class App extends Component {
 
-export default App;
+  render() {
+
+    return (
+            <div>
+              <Header />
+              </div>
+            )
+        }
+    }
+
+
+
+// const App = () => (
+//   <div className="ui container"> //to add margins to page
+//     <Route path="/" exact component={HomePage} />
+//     <Route path="/login" exact component={LoginPage} />
+//     </div>
+//   );
+//
+// export default App;
--- dream_connect_front/src/index.js
+++ dream_connect_front/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from 'react-router-dom';
+import  {BrowserRouter as Router, Route } from "react-router-dom"; // Routing
+//import {BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css' //library that helps in creation of form components
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('root'));
+ReactDOM.render(
+
+  <Router>
+        <Route exact path="/" component={App} />
+        //<Route exact path="/thehub" component={TheHub} />
+        //<Route exact path="/yourhub" component={YourHub} />
+
+    </Router>
+
+,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
